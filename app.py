5ddd4ab1da0fb6db3850271a55a518e7d7a78440
adventnopelesihite <47980343+adventnopelesihite@users.noreply.github.com>
2021-06-04 16:43:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 UPLOAD_FOLDER = 'static/uploads/'
@@ -15,51 +15,31 @@ app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
+ 
 @app.route('/')
 def index():
-    return render_template("index.html")
+   return render_template("index.html")
 
 @app.route('/', methods=['POST'])
 def post():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		
+		flash('Image successfully uploaded and displayed below')
+		flash('Namanya Jokowi')
+		return render_template('index.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
 
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-
-    if file not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-        file = request.files['file']
-
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-
-        
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-        flash('Image successfully uploaded and displayed below')
-        flash('Nama ' + filename)
-        return render_template('index.html', filename=filename)
-        
-
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        flash('Image successfully uploaded and displayed below')
-        flash('Namanya ' + filename)     
-        return render_template('index.html', filename=filename)
-        
-
-
-    else:
-        flash('Allowed image types are -> png, jpg, jpeg, gif')
-        return redirect(request.url)
-
-
-
-
+ 
 if __name__ == '__main__':
     app.run(debug=True)
